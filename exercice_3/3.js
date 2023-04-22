4ff7fc9dfb244fb52c6c2f78f3c517076f1633e2
isabelle913 <atelierisabelle@videotron.ca>
2023-04-22 08:02:33 -0400

    create function filterEvenNumbers

--- exercice_3/3.js
+++ exercice_3/3.js
@@ -1,5 +1,10 @@
 /*
 	Écrivez une fonction qui prend en entrée un tableau de nombres et retourne un nouveau tableau contenant uniquement les nombres pairs.
 */
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const filterEvenNumbers = numbersArray => { }
+const filterEvenNumbers = (numbersArray) => {
+  return (newArray = numbersArray.filter((nb) => nb % 2 === 0));
+};
+
+console.log(filterEvenNumbers(numbers));
