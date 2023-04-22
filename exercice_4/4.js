940088c0f3f657ed8b0db401e4462e5d4c097dbf
isabelle913 <atelierisabelle@videotron.ca>
2023-04-22 09:03:53 -0400

    create function countCharacters

--- exercice_4/4.js
+++ exercice_4/4.js
@@ -1,5 +1,28 @@
 /*
 	Écrivez une fonction qui prend en entrée une chaîne de caractères et retourne un objet avec la fréquence de chaque caractère. Ignorez les espaces, les caractères spéciaux et la casse (majuscules / minuscules).
 */
+const str = "Hello world 1 2 3!";
 
-const countCharacters = string => { }
+const countCharacters = (string) => {
+  const newString = string.replace(/[^a-zA-Z0-9_]/g, "").toLowerCase();
+  const responseObj = {};
+
+  for (let i = 0; i < newString.length; i++) {
+    // console.log(newString[i]);
+    // console.log(Object.keys(responseObj).includes(newString[i]));
+    if (Object.keys(responseObj).includes(newString[i])) {
+      // console.log("++");
+      responseObj[newString[i]]++;
+      // console.log(responseObj);
+    } else {
+      // console.log(newString[i]);
+      responseObj[newString[i]] = 1;
+      // console.log(responseObj);
+    }
+    // console.log("-------");
+  }
+  return responseObj;
+};
+
+console.log(countCharacters(str));
+countCharacters(str);
